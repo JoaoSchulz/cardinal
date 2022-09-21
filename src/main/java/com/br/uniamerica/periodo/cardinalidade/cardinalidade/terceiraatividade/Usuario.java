c90ab5258dff9bccd7494f6f2f0dde031f8c447c
JoaoSchulz <joaoguilherme.da@gmail.com>
2022-09-21 02:25:49 -0300

    atividade Cardinalidade

--- src/main/java/com/br/uniamerica/periodo/cardinalidade/cardinalidade/terceiraatividade/Usuario.java
+++ src/main/java/com/br/uniamerica/periodo/cardinalidade/cardinalidade/terceiraatividade/Usuario.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "td_usuario", schema = "terceiraatividade")
+@Table(name = "td_usuario", schema= "terceiraatividade")
 public class Usuario extends AbstractEntity{
 
     @Getter @Setter
